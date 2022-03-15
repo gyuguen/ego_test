3eab4a605654241e98d9d773deba19433fda63e9
gyuguen <gyuguen.jang@medibloc.org>
2022-03-15 17:20:37 +0900

    fix

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	panaceatypes "github.com/medibloc/panacea-core/v2/x/market/types"
 )
 
 func main() {
 	fmt.Println(sdk.AccAddress("panacea1dw3ev60v44q68qrrsz8lmwrx68f70hyrykw5ry").String())
+	fmt.Println(panaceatypes.Deal{})
 }
